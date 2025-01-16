16edfc4c762e270b78268bbbdcd6eb623c61b5a1
Taisuke Fukuno <fukuno@jig.jp>
2025-01-17 07:46:28 +0900

    fix for lines with comma

--- makeCSV.js
+++ makeCSV.js
@@ -12,19 +12,19 @@ const srcs = {
 const list = [["word", "description", "src_title", "src_url"]];
 let src = null;
 for (const s of data) {
-  const ss = s.split(",");
-  if (ss.length == 0) continue;
-  if (ss.length == 1) {
-    if (ss[0] == "") continue;
+  if (s.length == 0) continue;
+  const n = s.indexOf(",");
+  if (n < 0) {
     for (const name in srcs) {
-      if (ss[0].startsWith(name)) {
+      if (s.startsWith(name)) {
         src = srcbase + srcs[name];
         break;
       }
     }
-    console.log("src", src, "<-", ss[0]);
+    console.log("src", src, "<-", s);
     continue;
   }
+  const ss = [s.substring(0, n), s.substring(n + 1)];
   ss.push("さくらのクラウド検定オンライン教材");
   ss.push(src);
   list.push(ss);
